ae4a50b521a5d493dbd0dac6a003bf4ab2a337d9
jvas04 <cortelamp2004@gmail.com>
2024-04-11 08:13:43 -0500

    Commit Repositories

---
+++ Veterinary.WEB/Repositories/Repository.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using System.Text;
+
+namespace Veterinary.WEB.Repositories
+{
+    public class Repository : IRepository
+    {
+        private readonly HttpClient httpClient;
+
+        private JsonSerializerOptions _jsonSerializerOptions => new JsonSerializerOptions
+        {
+
+            PropertyNameCaseInsensitive = true
+
+        };
+
+
+        public Task<HttpResponseWrapper<T>> GetAsync<T>(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseWrapper<object>> Post<T>(string url, T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
