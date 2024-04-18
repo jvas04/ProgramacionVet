063fa12d6fa722daf9e913ef00ce09972884381c
jvas04 <cortelamp2004@gmail.com>
2024-04-18 09:59:39 -0500

    Commit OwnerForm - SweetAlert 2

--- Veterinary.WEB/Repositories/IRepository.cs
+++ Veterinary.WEB/Repositories/IRepository.cs
@@ -6,6 +6,15 @@
         Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
         Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);
 
+        Task <HttpResponseWrapper<object>>DeleteAsync(string url);
+
+
+        Task<HttpResponseWrapper<object>> PutAsync<T>(string url, T model);
+        Task<HttpResponseWrapper<TResponse>> PutAsync<T, TResponse>(string url, T model);
+
+
+
+
 
     }
 }
